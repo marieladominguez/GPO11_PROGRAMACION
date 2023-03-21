0173155a54b6be4b238d765d03616cf662eb56b1
german <germangustavocano@gmail.com>
2023-03-21 14:42:54 -0300

    imc

---
+++ PROYECTOS/IMC/JS/script.js
@@ -0,0 +1,13 @@
+// ARMAR LA VALIDACION DEL JS PARA HACER EL CALCULO Y MOSTRAR POR PANTALLA
+
+/*let nombre= document.getElementById(nombre)
+alert("HOLA", nombre)
+-------------------------------------------------------
+let altura= document.getElementById(altura)
+let peso= document.getElementById(peso)
+let resultado
+function calculo() {   
+    resultado=peso/(altura*altura)
+    alert("tu imc es", resultado)
+}
+calculo()*/
